ff4beca9d06d2ccbbd1bfbdf712a16abed959a12
AtaoGit <lanton2002@163.com>
2026-06-04 09:03:41 +0800

    chore: 移除 ExtraTrustStoreInitializer 类及相关配置

--- src/main/java/com/ywt/passage/AiPassageCreatorApplication.java
+++ src/main/java/com/ywt/passage/AiPassageCreatorApplication.java
@@ -1,6 +1,5 @@
 package com.ywt.passage;
 
-import com.ywt.passage.config.ExtraTrustStoreInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,12 +7,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class AiPassageCreatorApplication {
-    
+
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(AiPassageCreatorApplication.class);
-        application.addInitializers(new ExtraTrustStoreInitializer());
-        application.run(args);
+        SpringApplication.run(AiPassageCreatorApplication.class, args);
         System.out.println("Service Start Successful ~~");
     }
-    
+
 }
